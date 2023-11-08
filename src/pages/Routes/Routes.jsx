fda0ee63151fa56d661ad4708f635156a8c52b6f
himaloyash <himaloyashis98@gmail.com>
2023-11-08 23:43:19 +0600

    almost done

--- src/pages/Routes/Routes.jsx
+++ src/pages/Routes/Routes.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../Layout/Main/Main';
+import Home from '../Home/Home';
 
-const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
 
-export default Routes;
+export default router;
