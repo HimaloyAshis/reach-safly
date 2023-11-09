c6053ac9988f1b567755e3c9de86cfc15a40a27e
himaloyash <himaloyashis98@gmail.com>
2023-11-10 01:15:21 +0600

    good night

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className=''>
-            <div className=' bg-[#1746A2] py-6 px-4' style={{width:''}}>
+            <div className=' bg-[#1746A2] py-6 px-4'>
                 <div className='md:flex justify-between items-center'>
                     <div className=' text-[#] w-2/6'>
                         <p className=' font-bold  text-[#F4CE14] text-3xl p '>Explore the </p>
@@ -16,9 +16,23 @@ const Home = () => {
                         <img className='w-80' src="https://img.freepik.com/free-photo/traveler-girl-searching-right-direction-map_1150-45010.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.1842592228.1683275805&semt=ais" alt="" />
                     </div>
                 </div>
-                <div className='flex-col'>
-                    <label htmlFor="Destination" className='text-white rounded flex-col'>Destination</label>
-                    <input type="text" name="" placeholder='Where to go' className='tex-[#]' id="" />
+                <div className='md:flex gap-3 bg-[#F5F7F8] -top-44 justify-center flex items-center p-4'>
+                    <div className='flex flex-col  w-48'>
+                        <label htmlFor="Destination " className='text-black rounded'>Destination</label>
+                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
+                    </div>
+                    <div className='flex flex-col  w-48'>
+                        <label htmlFor="Destination " className='text-black rounded'>Destination</label>
+                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
+                    </div>
+                    <div className='flex flex-col  w-48'>
+                        <label htmlFor="Destination " className='text-black rounded'>Destination</label>
+                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
+                    </div>
+                    <div className='flex flex-col  w-48'>
+                        <label htmlFor="Destination " className='text-black rounded'>Destination</label>
+                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
+                    </div>
                 </div>
             </div>
             <div></div>
