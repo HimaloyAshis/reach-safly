e6b8f1670766868b1d0cac3759cdcecfff07be46
himaloyash <himaloyashis98@gmail.com>
2023-11-10 11:33:09 +0600

    category almost done

--- src/pages/BrowsByCategory/BrowsByCategory.jsx
+++ src/pages/BrowsByCategory/BrowsByCategory.jsx
@@ -3,16 +3,48 @@ import { MdTour } from "react-icons/md";
 
 const BrowsByCategory = () => {
     return (
-        <div >
+        <div className=' bg-[#e2e7ea] pt-16 '>
             <div className='flex items-center justify-center'>
                 <div>
-                    <h1>Brows By Category</h1>
-                    <p className='font-bold text-xl  '>Pick a tour type</p>
+                    <h1 className='font-bold text-xl '>Brows By Category</h1>
+                    <p className='font-bold  text-3xl '>Pick a tour type</p>
                 </div>
             </div>
-            <div>
-                <div>
-                    <MdTour></MdTour>
+            <div className='md:flex gap-16 pt-10 sm:gap-8 justify-center items-center'>
+                <div className='bg-[#fff] h-36 w-36 flex justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                        <MdTour className='h-14 flex justify-center items-center w-14 rounded-full p-1 bg-[#F4CE14] '></MdTour>
+                        <p className='font-bold text-xl '>City Tours</p>
+                        <p className='text-[#8d989e]'>10 Tours +</p>
+                    </div>
+                </div>
+                <div className='bg-[#fff] h-36 w-36 flex justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                        <MdTour className='h-14 flex justify-center items-center w-14 rounded-full p-1 bg-[#F4CE14] '></MdTour>
+                        <p className='font-bold text-xl '>City Tours</p>
+                        <p className='text-[#8d989e]'>10 Tours +</p>
+                    </div>
+                </div>
+                <div className='bg-[#fff] h-36 w-36 flex justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                        <MdTour className='h-14 flex justify-center items-center w-14 rounded-full p-1 bg-[#F4CE14] '></MdTour>
+                        <p className='font-bold text-xl '>City Tours</p>
+                        <p className='text-[#8d989e]'>10 Tours +</p>
+                    </div>
+                </div>
+                <div className='bg-[#fff] h-36 w-36 flex justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                        <MdTour className='h-14 flex justify-center items-center w-14 rounded-full p-1 bg-[#F4CE14] '></MdTour>
+                        <p className='font-bold text-xl '>City Tours</p>
+                        <p className='text-[#8d989e]'>10 Tours +</p>
+                    </div>
+                </div>
+                <div className='bg-[#fff] h-36 w-36 flex justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                        <MdTour className='h-14 flex justify-center items-center w-14 rounded-full p-1 bg-[#F4CE14] '></MdTour>
+                        <p className='font-bold text-xl '>City Tours</p>
+                        <p className='text-[#8d989e]'>10 Tours +</p>
+                    </div>
                 </div>
             </div>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,26 +13,26 @@ const Home = () => {
                         <p className='  text-[#949ea3] text-xl p'>Find awesome hotel, car and activities in london </p>
                     </div>
                     <div className='md:flex justify-center pt-10 gap-8 '>
-                        <img className='w-80 h-96' src="https://img.freepik.com/free-photo/woman-hand-holding-camera-standing-top-rock-nature-travel-concept_335224-887.jpg?w=900&t=st=1699552162~exp=1699552762~hmac=7ad333c91b316db014d61a35560af665d8f15e4d6c0011743d243ee60206e7c4" alt="" />
+                        <img className='w-96 h-96' src="https://img.freepik.com/free-photo/tourist-sitting-phu-sub-lek-viewpoint-sunset-lopburi-thailand_335224-1390.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.1842592228.1683275805&semt=ais" alt="" />
                         <img className='w-80' src="https://img.freepik.com/free-photo/traveler-girl-searching-right-direction-map_1150-45010.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.1842592228.1683275805&semt=ais" alt="" />
                     </div>
                 </div>
                 <div className='md:flex gap-8 rounded-md bg-[#F5F7F8] md:w-3/3 justify-center items-center p-4' style={{position:'absolute', top:'470px'}}>
                     <div className='flex flex-col  w-48'>
                         <label htmlFor="Destination " className='text-black rounded'>Destination</label>
-                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
+                        <input type="text" name="" placeholder='Where to go' className='text-[#F5F7F8] rounded p-1 ' id="" />
+                    </div>
+                    <div className='flex flex-col  w-48'>
+                        <label htmlFor="Destination " className='tt-black rounded'>Destination</label>
+                        <input type="text" name="" placeholder='Where to go' className='text-[#F5F7F8] rounded p-1 ' id="" />
                     </div>
                     <div className='flex flex-col  w-48'>
                         <label htmlFor="Destination " className='text-black rounded'>Destination</label>
-                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
+                        <input type="text" name="" placeholder='Where to go' className='text-[#F5F7F8] rounded p-1 ' id="" />
                     </div>
                     <div className='flex flex-col  w-48'>
                         <label htmlFor="Destination " className='text-black rounded'>Destination</label>
-                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
-                    </div>
-                    <div className='flex flex-col  w-48'>
-                        <label htmlFor="Destination " className='text-black rounded'>Destination</label>
-                        <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
+                        <input type="text" name="" placeholder='Where to go' className='text-[#F5F7F8] rounded p-1 ' id="" />
                     </div>
                     <button type='submit' className='bg-[#FF9209] text-white py-1 px-3 rounded'>Search</button>
                 </div>
