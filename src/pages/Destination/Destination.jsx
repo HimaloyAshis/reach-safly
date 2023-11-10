daab70f0b9da0f78cb6826761f9d178efec53438
himaloyash <himaloyashis98@gmail.com>
2023-11-10 20:58:19 +0600

    height setup

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import '../Destination/Destination.css'
 
 const Destination = () => {
     return (
-        <div className='mt-16 '>
+        <div className='mt-16 ' >
             <p className='font-bold text-2xl'>Destination</p>
             <p className='font-bold text-2xl pb-6'>Top Destinations</p>
             <Carousel>
                 <div>
-                    <img src="https://images.pexels.com/photos/18969427/pexels-photo-18969427/free-photo-of-forth-bridge-in-scotland.jpeg?auto=compress&cs=tinysrgb&w=600" />
+                    <img className=''  src="https://images.pexels.com/photos/18969427/pexels-photo-18969427/free-photo-of-forth-bridge-in-scotland.jpeg?auto=compress&cs=tinysrgb&w=600" />
 
                 </div>
                 <div>
-                    <img src="assets/2.jpeg" />
+                    <img className=''  src="https://images.pexels.com/photos/18969427/pexels-photo-18969427/free-photo-of-forth-bridge-in-scotland.jpeg?auto=compress&cs=tinysrgb&w=600" />
 
                 </div>
                 <div>
-                    <img src="assets/3.jpeg" />
+                    <img className=''  src="https://images.pexels.com/photos/18969427/pexels-photo-18969427/free-photo-of-forth-bridge-in-scotland.jpeg?auto=compress&cs=tinysrgb&w=600" />
 
                 </div>
             </Carousel>
