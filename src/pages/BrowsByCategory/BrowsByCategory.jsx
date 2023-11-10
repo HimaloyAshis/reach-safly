3e17c4a70098ae20462b579f9e0f39d18214714b
himaloyash <himaloyashis98@gmail.com>
2023-11-10 11:07:19 +0600

    icons ok

--- src/pages/BrowsByCategory/BrowsByCategory.jsx
+++ src/pages/BrowsByCategory/BrowsByCategory.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { MdTour } from "react-icons/md";
 
 const BrowsByCategory = () => {
     return (
-        <div>
-            <h1>Brows By Category</h1>
-            <p className='font-bold text-xl  '>Pick a tour type</p>
-            
+        <div >
+            <div className='flex items-center justify-center'>
+                <div>
+                    <h1>Brows By Category</h1>
+                    <p className='font-bold text-xl  '>Pick a tour type</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <MdTour></MdTour>
+                </div>
+            </div>
+
         </div>
     );
 };
