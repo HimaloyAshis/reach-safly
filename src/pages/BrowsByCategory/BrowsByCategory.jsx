ac6f666dc35e5b6a22e13d7341a4d077828cd62d
himaloyash <himaloyashis98@gmail.com>
2023-11-10 09:22:28 +0600

    category

---
+++ src/pages/BrowsByCategory/BrowsByCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BrowsByCategory = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BrowsByCategory;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
                         <img className='w-80' src="https://img.freepik.com/free-photo/traveler-girl-searching-right-direction-map_1150-45010.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.1842592228.1683275805&semt=ais" alt="" />
                     </div>
                 </div>
-                <div className='md:flex gap-8 bg-[#F5F7F8] md:w-3/3 justify-center items-center p-4' style={{position:'absolute', top:'470px'}}>
+                <div className='md:flex gap-8 rounded-md bg-[#F5F7F8] md:w-3/3 justify-center items-center p-4' style={{position:'absolute', top:'470px'}}>
                     <div className='flex flex-col  w-48'>
                         <label htmlFor="Destination " className='text-black rounded'>Destination</label>
                         <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
@@ -33,7 +33,7 @@ const Home = () => {
                         <label htmlFor="Destination " className='text-black rounded'>Destination</label>
                         <input type="text" name="" placeholder='Where to go' className='tex-[#F5F7F8] rounded p-1 ' id="" />
                     </div>
-                    <button type='submit' className='bg-[#FF9209] text-white py-1 px-3'>Search</button>
+                    <button type='submit' className='bg-[#FF9209] text-white py-1 px-3 rounded'>Search</button>
                 </div>
             </div>
             <div></div>
